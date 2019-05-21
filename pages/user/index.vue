13335aefc9b20fe146a7c49d3eb538963fd11254
super5161 <Administrator@PC-201809171623>
2019-05-21 11:14:00 +0800

    Update index.vue
    修改标识

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -3,7 +3,7 @@
 		<navigator url="/pages/login/login" open-type="redirect" hover-class="none">
 			<button style="width: 50%;" type="warn">退出登录</button>
 		</navigator>
-		<page-foot tab="1"></page-foot>
+		<page-foot tab="3"></page-foot>
 	</view>
 </template>
 
